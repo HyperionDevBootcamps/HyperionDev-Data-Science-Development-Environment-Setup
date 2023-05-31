1c59d0b07a2780c258e285eeee851cb0da819524
Ronald <ronaldm@hyperiondev.com>
2023-05-31 10:30:20 +0200

    Make script clear

--- windows_ds.ps1
+++ windows_ds.ps1
@@ -1,4 +1,6 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+Invoke-Expression (Invoke-WebRequest -URI 'https://community.chocolatey.org/install.ps1').Content
 choco install python
 choco install git
 choco install vscode
